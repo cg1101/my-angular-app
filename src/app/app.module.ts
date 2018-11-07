e786d390097b7335ee62f8fd5fe4323468b1f3d1
cg1101 <cg1101@gmail.com>
2018-11-07 19:27:01 +1100

    add components to demonstrate route change doesn't cause component to be recreated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { AccountComponent } from './account/account.component';
+import { UsageComponent } from './usage/usage.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AccountComponent,
+  },
+  {
+    path: 'usage/:date',
+    component: UsageComponent,
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    UsageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    // ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/usage/usage.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-usage',
+  templateUrl: './usage.component.html',
+  styleUrls: ['./usage.component.css']
+})
+export class UsageComponent implements OnInit, OnDestroy {
+
+  dateSpec$: Observable<string>;
+  rows$: Observable<[string, number][]>;
+  rows: [string, number][];
+
+  constructor(public route: ActivatedRoute) {
+    console.log('UsageComponent.constructor()');
+    console.log(`route is`, this.route);
+    this.dateSpec$ = route.paramMap.pipe(
+      map(p => p.get('date')),
+    )
+  }
+
+  ngOnInit() {
+    console.log('UsageComponent.ngOnInit()');
+    this.rows$ = this.dateSpec$.pipe(
+      map(dateSpec => {
+        const rs = dateSpec === 'all' ? [
+          ['JAN-2018', 150],
+          ['FEB-2018', 200],
+        ] : [
+          ['customer 1', 100],
+          ['customer 2', 200],
+        ];
+        return rs as [string, number][];
+      }),
+    );
+    this.rows$.subscribe(rows => {
+      console.log('rows =>', rows);
+    });
+  }
+
+  ngOnDestroy() {
+    console.log('UsageComponent.ngOnDestroy()');
+  }
+
+}
